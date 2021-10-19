1b008f92c642c1cdca75fa49d9570d385bc623ef
Rimvydas Šimkus <84323090+Rimvydas1987@users.noreply.github.com>
2021-10-19 11:29:27 +0300

    small changes to LT

--- scooters/src/CountSorter.jsx
+++ scooters/src/CountSorter.jsx
@@ -8,7 +8,7 @@ function CountSorter({ sort, scootersCount, scootersCountRide }) {
         <button className="btn-sort" type="button" onClick={() => sort('total_ride_kilometres')}>Pagal nuvažiuotą atstumą</button>
       </div>
       <div className="statistics-conteiner">
-        <h5>Statistics</h5>
+        <h5>Statistika</h5>
         <h6>Viso paspirtukų: {scootersCount} </h6>
         <h6>Bendras nuvažiuotas atstumas: {scootersCountRide} km</h6>
       </div>
